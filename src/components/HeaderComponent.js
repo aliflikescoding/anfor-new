4cfd6e53b435eca428f498d50c14cadca8ed96b2
Alif Wibowo <alif.likes.coding@gmail.com>
2025-07-06 15:43:38 +0700

    Fix header styling

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -9,23 +9,25 @@ const HeaderComponent = () => {
 
   if (isSticky) {
     return (
-      <div className="fixed top-0 left-0 z-[99] w-full p-4 flex justify-between items-center transition-all">
-        <Link href={`/`}>
-          <p className="font-bold uppercase text-3xl">
-            anforcom <br /> 2025
-          </p>
-        </Link>
-        <Link href={`/`}>
-          <Image
-            src="/logo.svg"
-            alt="robot"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-auto"
-          />
-        </Link>
-        <button className="btn btn-neutral btn-custom">daftar</button>
+      <div className="fixed top-0 left-0 z-[99] w-full p-4 transition-all">
+        <div className="custom-container flex justify-between items-center">
+          <Link href={`/`}>
+            <p className="font-bold uppercase text-3xl">
+              anforcom <br /> 2025
+            </p>
+          </Link>
+          <Link href={`/`}>
+            <Image
+              src="/logo.svg"
+              alt="robot"
+              height={0}
+              width={0}
+              sizes="100vw"
+              className="h-auto w-auto"
+            />
+          </Link>
+          <button className="btn btn-neutral btn-custom">daftar</button>
+        </div>
       </div>
     );
   }
